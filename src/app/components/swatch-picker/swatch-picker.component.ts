f9348fa4d2562ddada6256f071882a7d941c7bf4
Andy Allsopp <arallsopp@gmail.com>
2023-09-15 17:49:52 +0100

    built out palette

--- src/app/components/swatch-picker/swatch-picker.component.ts
+++ src/app/components/swatch-picker/swatch-picker.component.ts
@@ -9,11 +9,117 @@ import { Component } from '@angular/core';
 export class SwatchPickerComponent {
 
   public palette = [{
-    name: 'black',
+    name: 'Black',
     color: "#000"
   },{
-    name: 'red',
-    color: "#FF0000"
-  }
+    name: 'Maple Brown',
+    color: "#674e26"
+  },{
+    name: 'Russet Brown',
+    color: "#864721"
+  },{
+    name: 'Sienna Brown',
+    color: "#855A25"
+  },{
+    name: 'Dolphin Grey',
+    color: "#828FA0"
+  },{
+    name: 'Gunmetal Grey',
+    color: "#6C7177"
+  },{
+    name: 'Phantom Grey',
+    color: "#818487"
+  },{
+    name: 'Slate Grey',
+    color: "#667988"
+  },{
+    name: 'Leyland White',
+    color: "#f1ffff"
+  },{
+    name: 'Burgundy',
+    color: "#762315"
+  },{
+    name: 'Carmine',
+    color: "#C100100"
+  },{
+    name: 'Cherry',
+    color: "#8a0e35"
+  },{
+    name: 'Matador',
+    color: "#C61E1E"
+  },{
+    name: 'Magenta',
+    color: "#7A2A6F"
+  },{
+    name: 'Pimento',
+    color: "#8a0e35"
+  },{
+    name: 'Scarlet',
+    color: "#9C1C13"
+  },{
+    name: 'Signal',
+    color: "#BB0200"
+  },{
+    name: 'Vermilion',
+    color: "#e64916"
+  },{
+    name: 'Inca',
+    color: "#FFE641"
+  },{
+    name: 'Jasmin',
+    color: "#FCFDB0"
+  },{
+    name: 'Jonquil',
+    color: "#e3cb9b"
+  },{
+    name: 'Mimosa',
+    color: "#F5C53C"
+  },{
+    name: 'Saffron',
+    color: "#FFD020"
+  },{
+    name: 'Topaz',
+    color: "#EE7831"
+  },{
+    name: 'Cactus',
+    color: "#D3E0C8"
+  },{
+    name: 'Conifer',
+    color: "#072522"
+  },{
+    name: 'Java',
+    color: "#009E74"
+  },{
+    name: 'British Racing Green',
+    color: "#007955"
+  },{
+    name: 'French Blue',
+    color: "#7396CC"
+  },{
+    name: 'Royal Blue',
+    color: "#152439"
+  },
+{
+    name: 'Tahiti',
+    color: "#003bd1"
+  },
+{
+    name: 'Valencia',
+    color: "#134F71"
+  },
+{
+    name: 'Wedgewood',
+    color: "#C2DEF4"
+  },
+{
+    name: 'Damson',
+    color: "#470F1C"
+  },
+{
+    name: 'Royal Blue',
+    color: "#"
+  },
+
+
   ]
 }
