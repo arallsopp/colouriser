709655a00e374c8b484523617f3988f321510cc8
Andy Allsopp <arallsopp@gmail.com>
2023-09-16 02:45:29 +0100

    added paint codes. looks good.

--- src/app/components/swatch-picker/swatch-picker.component.ts
+++ src/app/components/swatch-picker/swatch-picker.component.ts
@@ -18,110 +18,145 @@ export class SwatchPickerComponent {
 
   public palette = [{
     name: 'Black',
-    color: "#000"
+    color: "#000",
+    codes: '11'
   },{
     name: 'Maple Brown',
-    color: "#674e26"
+    color: "#674e26",
+    codes: '73, 83'
   },{
     name: 'Russet Brown',
-    color: "#864721"
+    color: "#864721",
+    codes: '93, 203'
   },{
     name: 'Sienna Brown',
-    color: "#855A25"
+    color: "#855A25",
+    codes: '23'
   },{
     name: 'Dolphin Grey',
-    color: "#828FA0"
+    color: "#828FA0",
+    codes: '48'
   },{
     name: 'Gunmetal Grey',
-    color: "#6C7177"
+    color: "#6C7177",
+    codes: '18'
   },{
     name: 'Phantom Grey',
-    color: "#818487"
+    color: "#818487",
+    codes: '38'
   },{
     name: 'Slate Grey',
-    color: "#667988"
+    color: "#667988",
+    codes: '68'
   },{
     name: 'Leyland White',
-    color: "#f1ffff"
+    color: "#f1ffff",
+    codes: '19, 206'
+
   },{
     name: 'Burgundy',
-    color: "#762315"
+    color: "#762315",
+    codes: '42'
   },{
     name: 'Carmine',
-    color: "#C10010"
+    color: "#C10010",
+    codes: '82, 209'
   },{
     name: 'Cherry',
-    color: "#8a0e35"
+    color: "#8a0e35",
+    codes: '22'
   },{
     name: 'Matador',
-    color: "#C61E1E"
+    color: "#C61E1E",
+    codes: '12'
   },{
     name: 'Magenta',
-    color: "#7A2A6F"
+    color: "#7A2A6F",
+    codes: '92'
   },{
     name: 'Pimento',
-    color: "#F22917"
+    color: "#F22917",
+    codes: '72, 204'
   },{
     name: 'Scarlet',
-    color: "#9C1C13"
+    color: "#9C1C13",
+    codes: '52'
   },{
     name: 'Signal',
-    color: "#BB0200"
+    color: "#BB0200",
+    codes: '32'
   },{
     name: 'Vermilion',
-    color: "#e64916"
+    color: "#e64916",
+    codes: '118'
   },{
     name: 'Inca',
-    color: "#FFE641"
+    color: "#FFE641",
+    codes: '94, 207'
   },{
     name: 'Jasmin',
-    color: "#FCFDB0"
+    color: "#FCFDB0",
+    codes: '34'
   },{
     name: 'Jonquil',
-    color: "#e3cb9b"
+    color: "#e3cb9b",
+    codes: '14'
   },{
     name: 'Mimosa',
-    color: "#F5C53C"
+    color: "#F5C53C",
+    codes: '64'
   },{
     name: 'Saffron',
-    color: "#FFD020"
+    color: "#FFD020",
+    codes: '64'
   },{
     name: 'Topaz',
-    color: "#EE7831"
+    color: "#EE7831",
+    codes: '84'
   },{
     name: 'Cactus',
-    color: "#D3E0C8"
+    color: "#D3E0C8",
+    codes: '15'
   },{
     name: 'Conifer',
-    color: "#072522"
+    color: "#072522",
+    codes: '25'
   },{
     name: 'Java',
-    color: "#009E74"
+    color: "#009E74",
+    codes: '85, 205'
   },{
     name: 'BRG',
-    color: "#007955"
+    color: "#007955",
+    codes: '55'
   },{
     name: 'French Blue',
-    color: "#7396CC"
+    color: "#7396CC",
+    codes: '126'
   },{
     name: 'Royal Blue',
-    color: "#152439"
+    color: "#152439",
+    codes: '56, 156'
   },
 {
     name: 'Tahiti',
-    color: "#003bd1"
+    color: "#003bd1",
+    codes: '146'
   },
 {
     name: 'Valencia',
-    color: "#134F71"
+    color: "#134F71",
+    codes: '66'
   },
 {
     name: 'Wedgewood',
-    color: "#C2DEF4"
+    color: "#C2DEF4",
+    codes: '26'
   },
 {
     name: 'Damson',
-    color: "#470F1C"
+    color: "#470F1C",
+    codes: '17'
   }
   ]
 }
